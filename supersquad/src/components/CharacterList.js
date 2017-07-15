a856fb150153a75dc0abd04685738ad3b727cb13
Luke Lan <luke84320136@gmail.com>
2017-07-15 23:00:31 +0800

    React-Redux App

---
+++ supersquad/src/components/CharacterList.js
@@ -0,0 +1,42 @@
+import  React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { addCharacterById } from '../actions';
+
+class CharacterList extends  Component {
+    render () {
+        console.log('this.props',this.props);
+        return(
+            <div>
+                <h4>超級英雄</h4>
+                <ul>
+                    {this.props.characters.map(character => {
+                        return(
+                        <li key={character.id}>
+                        <div>{character.name}</div>
+                        <div
+                            onClick={() => this.props.addCharacterById(character.id)}
+                        >
+                         +
+                        </div>
+                        </li>
+                        )
+                    })
+                }
+                </ul>
+            </div>
+        )
+    }
+}
+
+function mapStateToProps(state) {
+    return {
+        characters: state.characters
+    };
+}
+
+// function mapDispatchToProps(dispatch) {
+//     return bindActionCreators ({addCharacterById}, dispatch)
+// }
+
+export default connect(mapStateToProps, {addCharacterById})(CharacterList);
